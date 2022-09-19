0a7ee5cc47528edea9e82d56c90da4956e4e54c8
Abdulrauf Yahaya <yabdulrauf911@gmail.com>
2022-09-19 15:17:35 -0700

    Pointers tasks 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - a ffnc that swapsthe valuesof 2 ints.
+ * @a: an int pointer
+ * @b: an int pointer
+ * Return - Nothing
+ */
+void swap_int(int *a, int *b);
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
