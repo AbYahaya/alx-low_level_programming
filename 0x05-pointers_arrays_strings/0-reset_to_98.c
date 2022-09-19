ad981a4b79af0fdae2116db70ffce363f206e9a7
Abdulrauf Yahaya <yabdulrauf911@gmail.com>
2022-09-19 07:23:12 -0700

    Pointersfirst task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pilointer parameter and update it's value
+ * pointing to 98
+ * @n: an integer value
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}	
