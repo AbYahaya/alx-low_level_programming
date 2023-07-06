a7177e176bea377475ed257d6f2fb567156fbd9f
AbYahaya <yabdulrauf911@gmail.com>
2023-07-06 13:15:56 -0700

    task 4 in first attempt

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a functon that returns the value of g raised to pw of h
+ * @g: an input integer  num
+ * @h: an input interger num
+ * Return:returns the value of g raised the power of h
+ */
+int _pow_recursion(int g, int h)
+{
+	if (h < 0)
+		return (-1);
+	else if (h == 0)
+		return (1);
+	else if (h == 1)
+		return (g);
+	else
+		return (x *= _pow_recursion(g, h - 1));
+}
