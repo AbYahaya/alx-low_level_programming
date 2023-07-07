83503bd88ef3fe6b8746227c4f477622c090e76a
AbYahaya <yabdulrauf911@gmail.com>
2023-07-07 02:06:05 -0700

    Changes to hello world task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 
 /**
  * Main - this will print the string in  the printf function
- * Desciption - using the main funtion 
+ * Desciption: using the main funtion
+ * Return: returns an int 
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("%s","with proper grammar, but the outcome is a piece of art\n");
 	return(0);
 }		
