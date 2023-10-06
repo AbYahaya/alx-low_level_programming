8b9fa8e4e72e788a64e830948e638fed083fd5dc
AbYahaya <yabdulrauf911@gmail.com>
2023-10-06 05:09:13 -0700

    A lil correction

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,10 +7,11 @@
  * @argv: arg vector
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i;
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
 	return (0);
 }
