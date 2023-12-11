0ddf8b745489597db6e9acfb6ccadedb702b84d1
AbYahaya <yabdulrauf911@gmail.com>
2023-12-11 03:28:05 -0800

    Task 0

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _atoi - a function that converts string to int
+ * @s: a string
+ * Return: an int
+ */
+int _atoi(char *s)
+{
+	while(*s)
+	{
+		return 0;
+	}	
+	return 0;
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - checks code
+ * @r: an int
+ * Description:returns absolute value
+ * Return: Absolute value of number r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
