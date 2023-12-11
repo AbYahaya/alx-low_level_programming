773ec663858710edeb60cfc4aa0c0ed8d239247c
AbYahaya <yabdulrauf911@gmail.com>
2023-12-11 04:10:47 -0800

    Task 2

---
+++ 0x18-dynamic_libraries/testfun.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - to add two numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: sum of both numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
